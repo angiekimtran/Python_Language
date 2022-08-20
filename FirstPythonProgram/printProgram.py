fc8b25a11bfae08a981b3c9bac8a67ca8f33dab8
angiekimtran <tranangiekim@gmail.com>
2022-08-19 21:31:15 -0600

    added comment

--- FirstPythonProgram/printProgram.py
+++ FirstPythonProgram/printProgram.py
@@ -1,7 +1,9 @@
+#This is my first Python program!
 print('Hello, world! My name is Angie Tran!\nI\'m 22 years old!')
 print("I'm 5'2!")
 print('''
-MY HOBBIES ARE:
-Singing
-Eating Out
-Listening To Music ''')
+    MY HOBBIES ARE:
+    Singing
+    Eating Out
+    Listening To Music 
+''')
