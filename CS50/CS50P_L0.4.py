1c2835203f2892e7e6eabd8dac016941617df208
angiekimtran <tranangiekim@gmail.com>
2022-09-15 15:55:32 -0600

    simple calculator with float

---
+++ CS50/CS50P_L0.4.py
@@ -0,0 +1,41 @@
+# simple calculator with float
+
+# Practice 4 - floating-point arithmetic and rounding function
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+# round(number{, ndigits})
+z = round(x + y)
+
+print(z)
+
+# Practice 5
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+z = round(x + y)
+
+# numeric formatting where comma is added every triple of digits
+# for example: 1,000
+print(f"{z:,}")
+
+# Practice 6 - round function format 1
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+# round(number{, ndigits}) 
+# round result to one decimal point
+z = round(x / y, 1)
+
+print(z)
+
+# Practice 7 - round function format 2
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+z = x / y
+
+# round result to two decimal point
+print(f"{z:.2f}")
+
+
