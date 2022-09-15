d2582a61a98ef6161f1988031bfdead3529f6b90
angiekimtran <tranangiekim@gmail.com>
2022-09-15 15:54:17 -0600

    simple calculator with int

---
+++ CS50/CS50P_L0.3.py
@@ -0,0 +1,23 @@
+# simple calculator with int, input, and nested functions
+
+# Practice 1
+x = 1
+y = 2
+
+z = x + y
+
+print(z)
+
+# Practice 2 - input function and string to int data type conversion
+x = input("What's x? ")
+y = input("What's y? ")
+
+z = int(x) + int(y)
+
+print(z)
+
+# Practice 3 - nested functions
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+print(x + y)
