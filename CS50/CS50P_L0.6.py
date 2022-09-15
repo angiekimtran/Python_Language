ca2fecca2db2c7aa8bc61c5d74133cece2d36ce2
angiekimtran <tranangiekim@gmail.com>
2022-09-15 16:01:48 -0600

    simple calculator with def and return function

---
+++ CS50/CS50P_L0.6.py
@@ -0,0 +1,10 @@
+# simple calculator with def and return function
+
+def main():
+    x = int(input("What's x?"))
+    print("x squared is", square(x))
+
+def square(n):
+    return n * n # can also be writen n ** 2 or pow(n, 2)
+
+main()
