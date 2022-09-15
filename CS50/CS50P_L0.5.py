bb85397b5f593236d73cc29dbffe7667eccb583f
angiekimtran <tranangiekim@gmail.com>
2022-09-15 15:57:24 -0600

    def function

---
+++ CS50/CS50P_L0.5.py
@@ -0,0 +1,22 @@
+# def function 
+
+# Practice 1 - poor formatting
+def hello():
+    print("Hello")
+
+name = input("What's your name? ")
+hello()
+print(name)
+
+# Practice 2 - better formatting
+def hello(to):
+    print("Hello", to)
+
+name = input("What's your name? ")
+hello(name)
+
+# Practice 3
+def hello(to="world"):
+    print("Hello", to)
+
+hello()
