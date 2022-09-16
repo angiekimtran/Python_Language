ef77a1caf15b313423f7d5b8bf76b01bb77c4617
angiekimtran <tranangiekim@gmail.com>
2022-09-16 15:26:49 -0600

    logical and relational operators program

---
+++ CS50/CS50P_L1.2.py
@@ -0,0 +1,30 @@
+# program comparing values with logical and relational operators
+
+# logical operators: [and, or, not]
+
+# Program 4 with or
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y or x > y:
+    print("x is not equal to y")
+else: 
+    print("x is equal to y")
+
+# Program 4 with !=
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x != y:
+    print("x is not equal to y")
+else: 
+    print("x is equal to y")
+
+# Program 5 with ==
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x == y:
+    print("x is equal to y")
+else: 
+    print("x is not equal to y")
