f2cd45a307d89d5351438d750d8d3bc0d4e2f079
angiekimtran <tranangiekim@gmail.com>
2022-09-17 23:31:29 -0600

    grading system program

---
+++ CS50/CS50P_L1.3.py
@@ -0,0 +1,40 @@
+# below are different ways to code a grading system program 
+
+# Program 1 
+score = int(input("Score: "))
+
+if score >= 90 and score <= 100:
+    print("Grade: A")
+elif score >= 80 and score < 90:
+    print("Grade: B")
+elif score >= 70 and score < 80:
+    print("Grade: C")
+elif score >= 60 and score < 70:
+    print("Grade: D")
+else:
+    print("Grade: F")
+
+
+# Program 2 - simpler than program 1
+if 90 <= score <= 100:
+    print("Grade: A")
+elif 80 <= score < 90:
+    print("Grade: B")
+elif 70 <= score < 80:
+    print("Grade: C")
+elif 60 <= score < 70:
+    print("Grade: D")
+else:
+    print("Grade: F")
+
+# Program 3 - simpler than program 1 and 2
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
