855122489cc4947484cc5edf3d262b1ad7e6c3e1
angiekimtran <tranangiekim@gmail.com>
2022-09-18 00:00:23 -0600

    match case statement

---
+++ CS50/CS50P_L1.5.py
@@ -0,0 +1,51 @@
+# programs using a match case statement
+
+# Program 1
+name = input("What's your name?")
+
+if name == "Harry":
+    print("Gryffindor")
+elif name == "Hermione":
+    print("Gryffindor")
+elif name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
+
+# Program 2
+name = input("What's your name?")
+
+if name == "Harry" or name == "Hermione" or name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")
+
+# Program 3
+name = input("What's your name?")
+
+match name:
+    case "Harry":
+        print("Gryffindor")
+    case "Hermione":
+        print("Gryffindor")
+    case "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
+
+# Program 3
+name = input("What's your name?")
+
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
