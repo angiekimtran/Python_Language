52f3cb71970951e1a198dbbc008eea14cdc0fccd
angiekimtran <tranangiekim@gmail.com>
2022-09-18 21:56:59 -0600

    while loop

---
+++ CS50/CS50P_L2.0.py
@@ -0,0 +1,21 @@
+# practice program with while loop
+# while is a construct that allows a question to be asked again and again
+
+# Program 1 - prograam that prints out "meow" three times
+i = 3
+while i != 0:
+    print("meow")
+    i = i - 1
+
+# Program 2 - another way to code program 1
+i = 1
+while i <= 3:
+    print("meow")
+    i = i + 1
+
+# Program 3 - another way to code program 1
+# starts at 0 and goes up to but not through 3
+i = 0
+while i < 3:
+    print("meow")
+    i += 1
