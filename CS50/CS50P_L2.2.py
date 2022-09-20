b819891759de6248263c675a7674b92f1ee8c371
angiekimtran <tranangiekim@gmail.com>
2022-09-19 19:46:08 -0600

    validating user input with loops

---
+++ CS50/CS50P_L2.2.py
@@ -0,0 +1,21 @@
+# a program that utilizes loops while also validating user input
+
+# Program 1 -asking user input then printing "meow" n amount of times
+while True:
+    n = int(input("What's n? "))
+    if n < 0:
+        continue
+    else:
+        break
+
+for _ in range(n):
+    print("meow")
+
+# Program 2 - better way to code Program 1
+while True:
+    n = int(input("What's n? "))
+    if n > 0:
+        break
+
+for _ in range(n):
+    print("meow")
