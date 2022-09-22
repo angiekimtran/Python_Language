ea8b4c63609d6badc047467118bf4d084506d267
angiekimtran <tranangiekim@gmail.com>
2022-09-22 15:10:49 -0600

    random module - randint & shuffle

---
+++ CS50/CS50P_L4.2.py
@@ -0,0 +1,19 @@
+# Practice programs with the Random Module
+
+# Program 1 - random.randint(a,b)
+# Program that randomly generates an integer between a and b inclusive
+# The program below randomly generates a number between 1 and 10
+import random
+
+number = random.randint(1, 10)
+print(number)
+
+# Program 2 - random.shuffle(x)
+# Program that takes in a list of values and shuffles them up to randomize the order they are in
+# Below is a card shuffling program
+import random
+
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
