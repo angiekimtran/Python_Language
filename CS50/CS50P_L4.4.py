57f84bf2d57bc34304b06c0f3e1e56210a1424c3
angiekimtran <tranangiekim@gmail.com>
2022-09-23 19:43:21 -0600

    command-line arguments part 2

---
+++ CS50/CS50P_L4.4.py
@@ -0,0 +1,20 @@
+# Practice programs with command-line arguments, sys.argv, and slicing
+
+import sys
+
+# Program 1 - slices
+# To take a slice of a data structure like a list is to take a subset of it
+# Type into the command line terminal the following: python {enter file name} {enter n amount of first names}
+if len(sys.argv) < 2: 
+    sys.exit("Too few arguments") 
+
+for arg in sys.argv[1:]:
+    print("hello, my name is", arg)
+
+
+# Program 2 - slices
+if len(sys.argv) < 2: 
+    sys.exit("Too few arguments") 
+
+for arg in sys.argv[1:-1]: # Negative numbers has the effect of counting in the other direction from the end of the list.
+    print("hello, my name is", arg) # As a result, the last first name entered won't be counted
