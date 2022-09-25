81a369248612e4b3fb91ce548c5a5d15b6a84424
Angie Tran <110869771+angiekimtran@users.noreply.github.com>
2022-09-25 16:57:42 -0600

    validating user input with user-defined functions

--- CS50/CS50P_L2_4.py
+++ CS50/CS50P_L2_4.py
@@ -1,5 +1,5 @@
-# program that utilizes def function, while and for loop, and validates user input
-# the program below asks for user input then prints "meow" n amount of times
+# Program that utilizes def function, while and for loop, and validates user input
+# The program below asks for user input then prints "meow" n amount of times
 def main():
     number = get_number()
     meow(number)
@@ -19,11 +19,13 @@ main()
 # For the program above, we can still use break to break out of the loop but after the loop we must still return n
 # This is demonstrated in the commented code below
 # Notice how the return keyword below is outside of the loop but is still inside the function
+
 # def get_number():
 #     while True:
 #         n = int(input("What's n? "))
 #         if n > 0:
 #              break
 #     return n
+
 # Although we can use a break here, return is stronger than a break
 # Return not only breaks out of a loop but also returns a value for you
