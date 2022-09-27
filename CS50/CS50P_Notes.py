5008c139302c2da7f29fa9df0d8f4349802284ad
Angie Tran <110869771+angiekimtran@users.noreply.github.com>
2022-09-26 20:10:09 -0600

    Delete CS50P_Notes.py

--- CS50/CS50P_Notes.py
+++
@@ -1,9 +0,0 @@
-# Practice programs with the Statistics Module
-
-import statistics
-
-print(statistics.mean([100, 90]))
-
-print(statistics.median([1,2,3,4,5]))
-
-print(statistics.mode([1,1,2,3,4,4,4,5]))
