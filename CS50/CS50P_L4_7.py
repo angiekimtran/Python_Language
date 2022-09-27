9c28b4c0019e7bef230c4b2a504ac8211e8ab472
angiekimtran <tranangiekim@gmail.com>
2022-09-26 19:30:32 -0600

    custom libraries part 1

---
+++ CS50/CS50P_L4_7.py
@@ -0,0 +1,14 @@
+# Practice program builing custom libraries (Part 1)
+
+def main():
+    hello("world")
+    goodbye("world")
+
+def hello(name):
+    print(f"hello, {name}")
+
+def goodbye(name):
+    print(f"goodbye, {name}")
+
+if __name__ == "__main__":
+    main()
